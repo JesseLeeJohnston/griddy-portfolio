9ad4116ad94f6d7cf3fbd68fdb96a078d3dd344b
jesseleejohnston <jesseleejohnston@gmail.com>
2020-02-24 20:25:43 -0800

    Added some styling updates to the blog.

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,28 +1,79 @@
 <template>
   <layout>
     <div class="container">
-      <div class="blog-tile">
-        <a href="#">
-          <h2>This is a blog title</h2>
-        </a>
-        <p class="text-small text-muted">March 1, 2020</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo veritatis nulla reiciendis laboriosam, dolorem earum voluptate perspiciatis cupiditate rem voluptates voluptatem facere! Quia nisi totam dicta ex sint vitae nam!</p>
-      </div>
+      <g-link :to="link" class="blog-tile">
+        <div>
+          <h2>This is a title</h2>
+          <p class="text-small is-marginless">December 25, 2020</p>
+          <p
+            class
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia laudantium, perspiciatis cupiditate corrupti debitis impedit! Aliquid vero numquam omnis nihil ut? Adipisci ullam ea quisquam nihil, doloribus repellendus aliquam vero?</p>
+        </div>
+        <p class="text-small uppercase read-blog">
+          Read More
+          <img
+            src="../assets/images/icons/arrow-right-blk.svg"
+            alt
+            width="10px"
+            height="10px"
+          />
+        </p>
+      </g-link>
 
-      <div class="blog-tile">
-        <a href="#">
-          <h2>This is a blog title</h2>
-        </a>
-        <p class="text-small text-muted">March 1, 2020</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo veritatis nulla reiciendis laboriosam, dolorem earum voluptate perspiciatis cupiditate rem voluptates voluptatem facere! Quia nisi totam dicta ex sint vitae nam!</p>
-      </div>
+      <g-link :to="link" class="blog-tile">
+        <div>
+          <h2>This is a title</h2>
+          <p class="text-small is-marginless">December 25, 2020</p>
+          <p
+            class
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia laudantium, perspiciatis cupiditate corrupti debitis impedit! Aliquid vero numquam omnis nihil ut? Adipisci ullam ea quisquam nihil, doloribus repellendus aliquam vero?</p>
+        </div>
+        <p class="text-small uppercase read-blog">
+          Read More
+          <img
+            src="../assets/images/icons/arrow-right-blk.svg"
+            alt
+            width="10px"
+            height="10px"
+          />
+        </p>
+      </g-link>
 
-      <div class="blog-tile">
-        <a href="#">
-          <h2>This is a blog title</h2>
-        </a>
-        <p class="text-small text-muted">March 1, 2020</p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo veritatis nulla reiciendis laboriosam, dolorem earum voluptate perspiciatis cupiditate rem voluptates voluptatem facere! Quia nisi totam dicta ex sint vitae nam!</p>
+      <g-link :to="link" class="blog-tile">
+        <div>
+          <h2>This is a title</h2>
+          <p class="text-small is-marginless">December 25, 2020</p>
+          <p
+            class
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia laudantium, perspiciatis cupiditate corrupti debitis impedit! Aliquid vero numquam omnis nihil ut? Adipisci ullam ea quisquam nihil, doloribus repellendus aliquam vero?</p>
+        </div>
+        <p class="text-small uppercase read-blog">
+          Read More
+          <img
+            src="../assets/images/icons/arrow-right-blk.svg"
+            alt
+            width="10px"
+            height="10px"
+          />
+        </p>
+      </g-link>
+
+      <div class="pagination">
+        <div>
+          <a href="#" class="link">Previous</a>
+        </div>
+
+        <div style="display: flex; justify-content: space-between;">
+          <a href="#" class="link">1</a>
+          <a href="#" class="link">2</a>
+          <a href="#" class="link">3</a>
+          <a href="#" class="link">4</a>
+          <a href="#" class="link active">5</a>
+        </div>
+
+        <div style="text-align: right;">
+          <a href="#" class="link">Next</a>
+        </div>
       </div>
     </div>
   </layout>
@@ -44,39 +95,39 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-a {
-  color: #081014;
-}
 .blog-tile {
-  background-color: #ffffff;
+  background-color: var(--background-color);
   background-repeat: no-repeat;
-  /* min-height: 300px; */
+  color: var(--primary-brand);
   border-radius: 0.5rem;
-  /* display: flex;
+  display: flex;
   flex-direction: column;
-  justify-content: space-between; */
+  justify-content: space-between;
   transition: transform 500ms ease-in-out,
-    /* background-size 1500ms 200ms cubic-bezier(0.17, 0.67, 0.62, 0.99), */
-      background-position 1500ms 200ms cubic-bezier(0.17, 0.67, 0.62, 0.99);
-  box-shadow: 0 15px 30px 0 rgba(0, 0, 0, 0.1);
+    background-position 1500ms 200ms cubic-bezier(0.17, 0.67, 0.62, 0.99);
+  box-shadow: 0 10px 30px 0 rgba(0, 0, 0, 0.1);
   background-size: 100%;
-  padding: 1.5rem 1rem;
-  margin: 3rem 0;
+  padding: 1rem;
+  margin-bottom: 3.5rem;
   &:hover {
     transform: scale(0.98);
     /* background-size: 100%; */
     background-position: 50% 55%;
-    .project-details {
+    .read-blog {
       transform: translateY(0px);
       opacity: 1;
     }
   }
-  .blog-details {
+  .read-blog {
     transition: opacity 1.5s ease-in-out,
       transform 1.5s cubic-bezier(0.17, 0.67, 0.62, 0.99);
     opacity: 0;
     transform: translateY(3px);
     margin: 0 0 0 auto;
   }
+}
+.pagination {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 </style>
